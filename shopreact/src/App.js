c7a3e8d7772732116ce69612de2e501352de9634
Lev Dom <bogatov2002@list.ru>
2025-07-12 00:37:10 +0500

    120725

--- shopreact/src/App.js
+++ shopreact/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header  from './components/Header'
 import Footer from './components/Footer'
+import Items from './components/Items'
 
 class App extends React.Component {
   constructor(props){
@@ -9,31 +10,35 @@ class App extends React.Component {
       items:[
         {
           id:1,
-          title:"chair.jpg",
+          title: "Стулья",
+          image:"chair.jpg",
           desc:"Удобный стул для удобных людей",
           category:"chairs",
           price:'49.99'
         },
         {
           id:2,
-          title:"table.jpg",
-          desc:"Удобный стул для удобных людей",
+          title:"Столы",
+          image: "table.jpg",
+          desc:"Практичный стол для пактичных людей",
           category:"table",
           price:'149.99'
         },
         {
           id:3,
-          title:"chair.jpg",
-          desc:"Удобный стул для удобных людей",
-          category:"chairs",
-          price:'49.99'
+          title:"Диваны",
+          image:"sofa.jpg",
+          desc:"Мягкий диван для мягких людей",
+          category:"sofa",
+          price:'329.99'
         },
         {
           id:4,
-          title:"chair.jpg",
-          desc:"Удобный стул для удобных людей",
-          category:"chairs",
-          price:'49.99'
+          title:"Комоды",
+          image:"commode.jpg",
+          desc:"Компактный комод для компактных людей",
+          category:"commode",
+          price:'59.99'
         },
       ]
     }
@@ -42,6 +47,7 @@ class App extends React.Component {
       return(
       <div className='wrapper'>
       <Header/>
+      <Items items={this.state.items}/>
       <Footer/>
     </div>
     );
