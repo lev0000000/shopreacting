70fb98fcb4a5b52f12d0124ce6a6acbca5c55e84
Lev Rabota <bogatov2002@list.ru>
2025-07-11 16:18:23 +0500

    110725

--- shopreact/src/App.js
+++ shopreact/src/App.js
@@ -1,13 +1,51 @@
+import React from 'react'
 import Header  from './components/Header'
 import Footer from './components/Footer'
 
-function App() {
-  return (
-   <div className='wrapper'>
-    <Header/>
-    <Footer/>
-   </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      items:[
+        {
+          id:1,
+          title:"chair.jpg",
+          desc:"Удобный стул для удобных людей",
+          category:"chairs",
+          price:'49.99'
+        },
+        {
+          id:2,
+          title:"table.jpg",
+          desc:"Удобный стул для удобных людей",
+          category:"table",
+          price:'149.99'
+        },
+        {
+          id:3,
+          title:"chair.jpg",
+          desc:"Удобный стул для удобных людей",
+          category:"chairs",
+          price:'49.99'
+        },
+        {
+          id:4,
+          title:"chair.jpg",
+          desc:"Удобный стул для удобных людей",
+          category:"chairs",
+          price:'49.99'
+        },
+      ]
+    }
+  }
+  render() {
+      return(
+      <div className='wrapper'>
+      <Header/>
+      <Footer/>
+    </div>
+    );
+  }
 }
 
 export default App;
